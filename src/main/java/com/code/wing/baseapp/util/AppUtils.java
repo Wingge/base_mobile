d15a22b8f6c8f069419ffd3ecc395e8923c0f497
wing <wingzhong72@gmail.com>
2016-09-22 18:49:49 +0800

    add utils

---
+++ src/main/java/com/code/wing/baseapp/util/AppUtils.java
@@ -0,0 +1,323 @@
+package com.code.wing.baseapp.util;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningTaskInfo;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.res.TypedArray;
+import android.os.Environment;
+import android.util.DisplayMetrics;
+import android.view.inputmethod.InputMethodManager;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Anthony on 2016/1/25.
+ * Class Note:SwipeBackUtils class  for common operation of app
+ */
+public class AppUtils {
+
+    private AppUtils() {
+        throw new AssertionError();
+    }
+
+
+    @SuppressWarnings("deprecation")
+    /**
+     * whether application is in background
+     * <ul>
+     * <li>need use permission android.permission.GET_TASKS in Manifest.xml</li>
+     * </ul>
+     *
+     * @param context
+     * @return if application is in background return true, otherwise return false
+     */
+    public static boolean isApplicationInBackground(Context context) {
+        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<RunningTaskInfo> taskList = am.getRunningTasks(1);
+        if (taskList != null && !taskList.isEmpty()) {
+            ComponentName topActivity = taskList.get(0).topActivity;
+            if (topActivity != null && !topActivity.getPackageName().equals(context.getPackageName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * get screen size width in pixels
+     *
+     * @param context
+     * @return the pixels of the screen width
+     */
+    public static int getWidth(Activity context) {
+        DisplayMetrics dm = new DisplayMetrics();
+        context.getWindowManager().getDefaultDisplay().getMetrics(dm);
+        return dm.widthPixels;
+    }
+
+    /**
+     * get screen size height in pixels
+     *
+     * @param context
+     * @return the pixels of the screen height
+     */
+    public static int getHeight(Activity context) {
+        DisplayMetrics dm = new DisplayMetrics();
+        context.getWindowManager().getDefaultDisplay().getMetrics(dm);
+        return dm.heightPixels;
+    }
+
+
+    /**
+     * convert dip to px
+     *
+     * @param context
+     * @param dpValue
+     * @return
+     */
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * convert px to dip
+     *
+     * @param context
+     * @param pxValue
+     * @return
+     */
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * convert px to sp
+     *
+     * @param context
+     * @param pxValue
+     * @return
+     */
+    public static int px2sp(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * convert sp to px
+     *
+     * @param context
+     * @param spValue
+     * @return
+     */
+    public static int sp2px(Context context, float spValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (spValue * scale + 0.5f);
+    }
+
+    /**
+     * get the package information of the context
+     *
+     * @param context
+     * @return
+     */
+    public static PackageInfo getPackageInfo(Context context) {
+        PackageInfo info = null;
+        try {
+            info = context.getPackageManager().getPackageInfo(
+                    context.getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace(System.err);
+        }
+        if (info == null)
+            info = new PackageInfo();
+        return info;
+    }
+
+    /**
+     * whether the external storage (SDCard) exists
+     *
+     * @return
+     */
+    public static boolean isStorageExists() {
+        if (Environment.getExternalStorageState().equals(
+                Environment.MEDIA_MOUNTED)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * hide keyboard
+     *
+     * @param activity
+     */
+    public static void hidekeyboard(Activity activity) {
+        try {
+            ((InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE))
+                    .hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+    /**
+     * return if str is empty
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isEmpty(String str) {
+        if (str == null || str.length() == 0 || str.equalsIgnoreCase("null") || str.isEmpty() || str.equals("")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * get format date
+     *
+     * @param timemillis
+     * @return
+     */
+    public static String getFormatDate(long timemillis) {
+        return new SimpleDateFormat("yyyy年MM月dd日").format(new Date(timemillis));
+    }
+
+    /**
+     * decode Unicode string
+     *
+     * @param s
+     * @return
+     */
+    public static String decodeUnicodeStr(String s) {
+        StringBuilder sb = new StringBuilder(s.length());
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (c == '\\' && chars[i + 1] == 'u') {
+                char cc = 0;
+                for (int j = 0; j < 4; j++) {
+                    char ch = Character.toLowerCase(chars[i + 2 + j]);
+                    if ('0' <= ch && ch <= '9' || 'a' <= ch && ch <= 'f') {
+                        cc |= (Character.digit(ch, 16) << (3 - j) * 4);
+                    } else {
+                        cc = 0;
+                        break;
+                    }
+                }
+                if (cc > 0) {
+                    i += 5;
+                    sb.append(cc);
+                    continue;
+                }
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * encode Unicode string
+     *
+     * @param s
+     * @return
+     */
+    public static String encodeUnicodeStr(String s) {
+        StringBuilder sb = new StringBuilder(s.length() * 3);
+        for (char c : s.toCharArray()) {
+            if (c < 256) {
+                sb.append(c);
+            } else {
+                sb.append("\\u");
+                sb.append(Character.forDigit((c >>> 12) & 0xf, 16));
+                sb.append(Character.forDigit((c >>> 8) & 0xf, 16));
+                sb.append(Character.forDigit((c >>> 4) & 0xf, 16));
+                sb.append(Character.forDigit((c) & 0xf, 16));
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * convert time str
+     *
+     * @param time
+     * @return
+     */
+    public static String convertTime(int time) {
+
+        time /= 1000;
+        int minute = time / 60;
+        int second = time % 60;
+        minute %= 60;
+        return String.format("%02d:%02d", minute, second);
+    }
+
+    /**
+     * url is usable
+     *
+     * @param url
+     * @return
+     */
+    public static boolean isUrlUsable(String url) {
+        if (AppUtils.isEmpty(url)) {
+            return false;
+        }
+
+        URL urlTemp = null;
+        HttpURLConnection connt = null;
+        try {
+            urlTemp = new URL(url);
+            connt = (HttpURLConnection) urlTemp.openConnection();
+            connt.setRequestMethod("HEAD");
+            int returnCode = connt.getResponseCode();
+            if (returnCode == HttpURLConnection.HTTP_OK) {
+                return true;
+            }
+        } catch (Exception e) {
+            return false;
+        } finally {
+            connt.disconnect();
+        }
+        return false;
+    }
+
+    /**
+     * is url
+     *
+     * @param url
+     * @return
+     */
+    public static boolean isUrl(String url) {
+        Pattern pattern = Pattern.compile("^([hH][tT]{2}[pP]://|[hH][tT]{2}[pP][sS]://)(([A-Za-z0-9-~]+).)+([A-Za-z0-9-~\\/])+$");
+        return pattern.matcher(url).matches();
+    }
+
+    /**
+     * get toolbar height
+     *
+     * @param context
+     * @return
+     */
+    public static int getToolbarHeight(Context context) {
+        final TypedArray styledAttributes = context.getTheme().obtainStyledAttributes(
+                new int[]{android.R.attr.actionBarSize});
+        int toolbarHeight = (int) styledAttributes.getDimension(0, 0);
+        styledAttributes.recycle();
+
+        return toolbarHeight;
+    }
+}
